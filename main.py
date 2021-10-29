f9d7eaae117ccebe1c7b7bedbf138523af64fa6b
killer <73597961+killer1101@users.noreply.github.com>
2021-10-29 17:03:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,7 @@ student_df = df.loc[df['student_id']=="TRL_xsl"]
 
 print(student_df.groupby("level")["attempt"].mean())
 
-fig = go.Figure(go.Scatter(
-    x = student_df.groupby("level")["attempt"].mean(),
-    y = ['Level 1','Level 2', 'Level 3', 'Level 4'],
-    orientation = 'h'
-))
+mean = df.groupby(["student_id", "level"], as_index=False)["attempt"].mean()
+fig = px.scatter(mean, x="student_id", y="level", size="attempt", color="attempt")
 
 fig.show()
-mean = df.groupby(["student_id", "level"], as_index=False)["attempt"].mean() fig = px.scatter(mean, x="student_id", y="level", size="attempt", color="attempt") fig.show()
