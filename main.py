cdf0e86fb06dd765719d8feb5b2cb4ec5d88c6ec
killer <73597961+killer1101@users.noreply.github.com>
2021-10-29 17:02:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,3 +14,4 @@ fig = go.Figure(go.Scatter(
 ))
 
 fig.show()
+mean = df.groupby(["student_id", "level"], as_index=False)["attempt"].mean() fig = px.scatter(mean, x="student_id", y="level", size="attempt", color="attempt") fig.show()
